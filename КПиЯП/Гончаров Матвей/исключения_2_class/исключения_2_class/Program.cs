45bda2f4a47d9f9ae11a26bf27c66ff34ee85e89
Xhref <goncharovgithub8@gmail.com>
2021-12-19 14:31:33 +0300

    update

--- КПиЯП/Гончаров Матвей/исключения_2_class/исключения_2_class/Program.cs
+++ КПиЯП/Гончаров Матвей/исключения_2_class/исключения_2_class/Program.cs
@@ -15,22 +15,33 @@ namespace исключения_2_class
             {
                 Reis rs1 = new Reis(1, "standart", "minsk", "12:43", "13:20");
                 Reis rs2 = new Reis(2, "standart", "sitniki", "11:12", "12:40");
-                Reis rs3 = new Reis(3, "elit", "minsk", "10:25", "11:25");
+                Reis rs3 = new Reis(3, "elit", "minsk", "90:25", "11:25"); // 90:25 не может быть
                 Reis rs4 = new Reis(4, "standart", "minsk", "09:10", "14:20");
-                Reis rs5 = new Reis(5, "elit", "minsk", "08:05", "09:30"); // Сделать исключения елсли вводится не 09:25 а 9:25
+                Reis rs5 = new Reis(5, "elit", "minsk", "08:05", "09:30"); 
 
-                lst1.Add(rs1);
-                lst1.Add(rs2);
-                lst1.Add(rs3);
-                lst1.Add(rs4);
-                lst1.Add(rs5);
+                if (rs1.NumberReisa < 0 || rs2.NumberReisa < 0 || rs3.NumberReisa < 0 || rs4.NumberReisa < 0 || rs5.NumberReisa < 0)
+                {
+                    throw new SpravochSystemException("Номер рейса не может быть < 0");
+                }
+                else
+                {
+                    lst1.Add(rs1);
+                    lst1.Add(rs2);
+                    lst1.Add(rs3);
+                    lst1.Add(rs4);
+                    lst1.Add(rs5);
+                }
 
                 SpravochSystem listSprav = new SpravochSystem(lst1);
-                listSprav.printReisTime(70, "minsk");
+                listSprav.printReisTime(70, "minsk"); // 70 минут
             }
-            catch (SpravochSystemException ex)
+            catch (ArgumentException ex)
             {
                 Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            catch (SpravochSystemException ex2)
+            {
+                Console.WriteLine("Ошибка: " + ex2.Message);
             }
            
         }
--- КПиЯП/Гончаров Матвей/исключения_2_class/исключения_2_class/spravochSystem.cs
+++ КПиЯП/Гончаров Матвей/исключения_2_class/исключения_2_class/spravochSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions; // Для регулярок 
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace исключения_2_class
     class SpravochSystem
     {
         // Список Рейсов
-        private List<Reis> listReis;
+        private List<Reis> listReis = new List<Reis>(6);
         // Совойство
         public List<Reis> ListReis
         {
@@ -19,20 +20,35 @@ namespace исключения_2_class
         // Чисто принимаю список
         public SpravochSystem(List<Reis> listReis)
         {
-            this.listReis = listReis;   
+            if (listReis.Count < 6)
+            {
+                this.listReis = listReis;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Список рейсов не может состоять более чем из 5 рейсов");
+            }
+             
         }
         // Либо сами рейсы и записываю в список
         public SpravochSystem(params Reis[] x)
         {
-            for (int i = 0; i < x.Length; i++)
+            if (x.Length < 6)
             {
-                listReis.Add(x[i]);
+                for (int i = 0; i < x.Length; i++)
+                {
+                    listReis.Add(x[i]);
+                }
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Список рейсов не может состоять более чем из 5 рейсов");
             }
         }
-        // функция которая выводит доступные рейсы до введенного времени
-        int hour, minute, reserve_time;
+        // функция которая выводит доступные рейсы до введенного времени 
         public void printReisTime(int time, string punktNaznash_)
         {
+            int hour, minute, reserve_time;
             // Я решил сделать так
             // time = 22:12
             // hour = time[0] * 10 + time[1]
@@ -41,29 +57,44 @@ namespace исключения_2_class
 
             for (int i = 0; i < listReis.Count; i++)
             {
-                // Если подходит пункт назначения
-                if (listReis[i].PlaceNaznach == punktNaznash_)
+                try
                 {
-                    // Отбытия 
-                    hour = Int32.Parse(listReis[i].Otbitia[0].ToString()) * 10 + Int32.Parse(listReis[i].Otbitia[1].ToString());
-                    minute = Int32.Parse(listReis[i].Otbitia[3].ToString()) * 10 + Int32.Parse(listReis[i].Otbitia[4].ToString());
-                    reserve_time = minute + hour * 60;
-
-                    //Прибытия
-                    hour = Int32.Parse(listReis[i].Pribitia[0].ToString()) * 10 + Int32.Parse(listReis[i].Pribitia[1].ToString());
-                    minute = Int32.Parse(listReis[i].Pribitia[3].ToString()) * 10 + Int32.Parse(listReis[i].Pribitia[4].ToString());
-                    minute += hour * 60; // Тут уже минуты прибытия
-
-                    // (время прибытия - время отбытия)
-                    reserve_time = (minute - reserve_time);
-
-                    // Console.WriteLine($"listReis[{i}].PlaceNaznach ==== minuts {reserve_time}"); // это для проверки
-
-                    // Проверяю
-                    if (reserve_time < time) // time принимается в минутах
+                    Regex rg1 = new Regex(@"\w*((0\d|1\d|2[0-3]):([0-5][0-9])(.*?)?)");
+                    if (rg1.IsMatch(listReis[i].Otbitia) && rg1.IsMatch(listReis[i].Pribitia))
                     {
-                        Console.WriteLine(listReis[i]); // Там переопределенный ToStrign и выведет значения
+                        // Если подходит пункт назначения
+                        if (listReis[i].PlaceNaznach == punktNaznash_)
+                        {
+                            // Отбытия 
+                            hour = Int32.Parse(listReis[i].Otbitia[0].ToString()) * 10 + Int32.Parse(listReis[i].Otbitia[1].ToString());
+                            minute = Int32.Parse(listReis[i].Otbitia[3].ToString()) * 10 + Int32.Parse(listReis[i].Otbitia[4].ToString());
+                            reserve_time = minute + hour * 60;
+
+                            //Прибытия
+                            hour = Int32.Parse(listReis[i].Pribitia[0].ToString()) * 10 + Int32.Parse(listReis[i].Pribitia[1].ToString());
+                            minute = Int32.Parse(listReis[i].Pribitia[3].ToString()) * 10 + Int32.Parse(listReis[i].Pribitia[4].ToString());
+                            minute += hour * 60; // Тут уже минуты прибытия
+
+                            // (время прибытия - время отбытия)
+                            reserve_time = (minute - reserve_time);
+
+                            // Console.WriteLine($"listReis[{i}].PlaceNaznach ==== minuts {reserve_time}"); // это для проверки
+
+                            // Проверяю
+                            if (reserve_time < time) // time принимается в минутах
+                            {
+                                Console.WriteLine(listReis[i]); // Там переопределенный ToStrign и выведет значения
+                            }
+                        }
                     }
+                    else
+                    {
+                        throw new SpravochSystemException("Вы ввели не коректную строку со временем");
+                    }
+                }
+                catch (SpravochSystemException ex)
+                {
+                    Console.WriteLine("Ошибка: " + ex.Message);
                 }
             }
         }
