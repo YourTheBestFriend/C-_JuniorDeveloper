5d3bcf374b172e05930b35bf560e059c5bff68c5
Kernel Panic <80000258+YourTheBestFriend@users.noreply.github.com>
2022-02-17 21:59:24 +0300

    update

---
+++ КПиЯП/Гончаров Матвей 2 семестр/xml_test/xml_test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml;
+
+namespace xml_test
+{
+    class Program
+    {
+        
+        public static void Main()
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("test.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+
+            // root tag
+            XmlElement rootElem = xDoc.CreateElement("root");
+            
+            // child tags
+            XmlElement userElem = xDoc.CreateElement("user");
+            XmlElement companyElem = xDoc.CreateElement("company");
+            
+            // text
+            XmlText userElemText = xDoc.CreateTextNode("Mattew");
+            XmlText companyElemText = xDoc.CreateTextNode("ITCompany");
+
+            companyElem.AppendChild(companyElemText);
+            userElem.AppendChild(userElemText);
+
+            userElem.AppendChild(companyElem);
+            rootElem.AppendChild(userElem);
+            xRoot.AppendChild(rootElem);
+
+            xDoc.Save("test.xml");
+            Console.ReadKey();
+        }
+    }
+}
+           
