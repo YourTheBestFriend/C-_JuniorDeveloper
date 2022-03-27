70945e6fd18dd85915bd4cd85f33a79150cde6c9
Matthew <goncharovgithub8@gmail.com>
2022-03-27 19:01:26 +0300

    update threads tasks

--- КПиЯП/Гончаров Матвей 2 семестр/Lab_5_Threads/Lab_5_Threads/ColoredConsole.cs
+++ КПиЯП/Гончаров Матвей 2 семестр/Lab_5_Threads/Lab_5_Threads/ColoredConsole.cs
@@ -18,16 +18,24 @@ namespace Lab_5_Threads
     //    public static void Write(ConsoleColor color, String format, params object[] list)
     //    {
     //        Console.ForegroundColor = color;
-    //        Console.Write(format, list);
+    //        Console.Write(format, list); // Это обычный метод для печати без перехода на новую строку - а то я сразу глянул, подумал рекурсия)
     //    }
     //}
     // поправки 1.3
     public static class ColoredConsole
     {
-        private static object locker = new object();
+        /*
+         Оператор lock получает взаимоисключающую блокировку заданного объекта 
+         перед выполнением определенных операторов, а затем снимает блокировку. 
+         Во время блокировки поток, удерживающий блокировку, может снова поставить
+         и снять блокировку. Любой другой поток не может получить блокировку и ожидает ее 
+         снятия.
+         */
+
+        private static object locker = new object(); // Сам наш объект блокировки
+        // Цвет, строка, любое кол-во объектов которое может встать в строку (можно не только число), python --- 'Hello my friend {0}, I'm {1} years old!'.format("Matthew", 17)
         public static void WriteLine(ConsoleColor color, String format, params object[] list)
         {
-
             lock (locker)
             {
                 Console.ForegroundColor = color;
--- КПиЯП/Гончаров Матвей 2 семестр/Lab_5_Threads/Lab_5_Threads/Program.cs
+++ КПиЯП/Гончаров Матвей 2 семестр/Lab_5_Threads/Lab_5_Threads/Program.cs
@@ -11,34 +11,71 @@ namespace Lab_5_Threads
         public static void Main()
         {
             // 1.1 
-            ColoredConsole.WriteLine(ConsoleColor.Red, "2+2={0}", 4);
+            ColoredConsole.WriteLine(ConsoleColor.Red, "2+2={0}", 4); // используется interpolation - Интерполяция
             ColoredConsole.WriteLine(ConsoleColor.Green, "просто зеленый текст");
 
-            // 1.2
-            Timer timerRed = new Timer(ClockTick1, null, 0, 200);
-            Timer timerGreen = new Timer(ClockTick2, null, 0, 200);
-            Console.ReadLine();
+            // 1.2 В данном случае есть несоответсвия - При выводе например слова Red - оно может быть зеленого цвета и т.д
+//            Timer timerRed = new Timer(ClockTick1, null, 0, 200);  // 0 -первый старт, сразу начинается // 200 миллисекунд 
+//            Timer timerGreen = new Timer(ClockTick2, null, 0, 200);
+            Console.ReadLine(); // Для задержки после запуска таймера, можно заюзать еще Thread.Sleep
+            /*
+             Class Timer sealed - не наследуется
+             
+             это при запуске до поправки
 
-            // 1.3 Поправки в Class => ColoredConsole
+             там был вопрос
+             1. Я подумал это из-за конфликта вызова функции с одинаковым временем перезапуска - нет
+             2. Из-за того что слишком мало времени - нет
+             3. Посидев подумав я запустил каждый таймер по отдельности, и когда они работуют не вместе выяснил что когда Red всегда красный и Green всегда зеленый
+             4. Попробовал запустить без передачи object o, даже не запустилось
+             5. Потом я задался вопросом что же обозначет это вопросо object? state - если я правильно понял // width = house != null ? house.width : null ==== width = house?.width - работает как условный оператор, тип если house будет null  то house?.width будет null, если house не null то результат house?.width = house.width
+             6. Думаю что проблема явно в том что они одновременно использую метод Write класса ColoredConsole, и из-за этого возникает конфликт т.к они работают в одном потоке?
+             */
 
-            // 1.4
-            Timer timerRed_2 = new Timer(ClockTick, ConsoleColor.Red, 0, 200);
+
+            // 1.3 Поправки в Class => ColoredConsole, после поправки действительно все стало отображаться как надо
+
+            // 1.4 // class - System.Threading.Timer,
+            Timer timerRed_2 = new Timer(ClockTick, ConsoleColor.Red, 0, 200); // 200 миллисекунд 
             Timer timerGreen_2 = new Timer(ClockTick, ConsoleColor.Green, 0, 200);
             Timer timerYellow = new Timer(ClockTick, ConsoleColor.Yellow, 0, 200);
-            while (!Console.KeyAvailable)
+            
+            // // // link 2.2
+            //Timer timer4 = new Timer(ClockTick, ConsoleColor.Blue, 0, 200);
+            //Timer timer5 = new Timer(ClockTick, ConsoleColor.White, 0, 200);
+            ////Timer timer6 = new Timer(ClockTick, ConsoleColor.Cyan, 0, 200);
+            ////Timer timer7 = new Timer(ClockTick, ConsoleColor.Yellow, 0, 200);
+            ////Timer timer8 = new Timer(ClockTick, ConsoleColor.Yellow, 0, 200);
+            ////Timer timer9 = new Timer(ClockTick, ConsoleColor.Yellow, 0, 200);
+            ////Timer timer10 = new Timer(ClockTick, ConsoleColor.Yellow, 0, 200);
+
+            while (!Console.KeyAvailable) // Ждет пока не нажму на клавушу
             {
                 Thread.Sleep(50);
                 ColoredConsole.Write(ConsoleColor.Gray, ".");
             }
 
-            
+            // 2.1 Посмотреть диспетчер задач
 
+            // 2.2 // только она начинает печатать с последнего
+            /* 
+                1. 5 ядер - добавил еще чтобы было 5 таймеров, последовательность сохраняется, но добавил 10 таймеров на 5 ядер происходит печать в произвольном порядке
+                2. Я думаю что в одном потоке
+             */
+
+            // 2.3 число потоков в диспетчере значительно увеличелось
+
+            // 2.4 потыкал в системном мониторе
+
+            // 2.5 
         }
 
         // 1.2
+        // в таймере эти методы запускаются каждые 200 миллисекунд
+        // эти два метода передаются и являются - TimerCallback callback - delegate
         static void ClockTick1(object o)
         {
-            ColoredConsole.Write(ConsoleColor.Red, "Red ");
+            ColoredConsole.Write(ConsoleColor.Red, "Red "); 
         }
         static void ClockTick2(object o)
         {
