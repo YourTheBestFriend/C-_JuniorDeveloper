f8420c7df504d8d0e226c450ecb0d0efa4606243
Matthew <goncharovgithub8@gmail.com>
2022-05-23 14:19:17 +0300

    wpf object 21 vek

---
+++ КПиЯП/Гончаров Матвей 2 семестр/Create_Object_21vek_laptop_n/Create_Object_21vek_laptop_n/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Create_Object_21vek_laptop_n
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
