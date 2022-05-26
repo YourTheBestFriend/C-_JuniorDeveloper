25d50c6a5e426c682cb2b9f84d609893197be3ea
Matthew <goncharovgithub8@gmail.com>
2022-05-27 02:33:52 +0300

    complete create object wpf 21 vek LR27

--- КПиЯП/Гончаров Матвей 2 семестр/Create_Object_21vek_laptop_n/Create_Object_21vek_laptop_n/MainWindow.xaml.cs
+++ КПиЯП/Гончаров Матвей 2 семестр/Create_Object_21vek_laptop_n/Create_Object_21vek_laptop_n/MainWindow.xaml.cs
@@ -25,22 +25,57 @@ namespace Create_Object_21vek_laptop_n
             InitializeComponent();
         }
 
+        // Make 
+        private void GridSplitter_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            textboxcash.Text = cashslider.Value.ToString();
+        }
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            textboxcash.Text = Math.Round(cashslider.Value, 2).ToString();
+        }
+
+        // May you create object or not?
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // give toolkit proizv
+            // proizv
+            if (proizv_1.IsChecked == false && proizv_2.IsChecked == false && proizv_3.IsChecked == false && proizv_4.IsChecked == false && proizv_5.IsChecked == false && proizv_6.IsChecked == false && proizv_8.IsChecked == false)
+            { 
+                my_the_best_rich_box.AppendText("Ошибка в секции Производители");
+            }
+            else
+            {
+                // size screen
+                if (size_1.IsChecked == false && size_2.IsChecked == false && size_3.IsChecked == false)
+                {
+                    my_the_best_rich_box.AppendText("Ошибка в секции Разрешение экрана");
+                }
+                else 
+                {
+                    // processor
+                    if (proc_1.IsChecked == false && proc_2.IsChecked == false && proc_3.IsChecked == false)
+                    {
+                        my_the_best_rich_box.AppendText("Ошибка в секции Процессор");
+                    }
+                    else
+                    {
+                        my_the_best_rich_box.AppendText("Тут типо создан объект, ошибок не возникло");
+                    }
+                } 
+            }
+        }
+
+        private void textboxcash_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
-
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
 
         }
 
         private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void GridSplitter_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
         {
 
         }
