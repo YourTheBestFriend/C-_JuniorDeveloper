a2b9403f37864b1907fed3ba871feeb26b119f77
Matthew <goncharovgithub8@gmail.com>
2022-05-27 04:32:23 +0300

    :/

--- КПиЯП/Гончаров Матвей 2 семестр/database_gui/database_gui/MainWindow.xaml.cs
+++ КПиЯП/Гончаров Матвей 2 семестр/database_gui/database_gui/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,53 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;////////////////////////////////////////////////// 
 
 namespace database_gui
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+
+
     public partial class MainWindow : Window
     {
+        // RETURN connect string
+        public async Task ConnectViaStringDTBAsync(string nameDatabase)
+        {
+            boxConnectInfo.Text = "asdfasdfasdfasdfasdf";
+            if (string.IsNullOrEmpty(nameDatabase) == false)
+            {
+                // Создание подключения
+                SqlConnection connection = new SqlConnection($"Server=(localdb)\\mssqllocaldb;Database={nameDatabase};User Id=shindows10;password=;TrustServerCertificate=True");
+                try
+                {
+                    // Открываем подключение
+                    await connection.OpenAsync();
+                    boxConnectInfo.Text = "Подключение открыто";
+                }
+                catch (SqlException ex)
+                {
+                    boxConnectInfo.Text = ex.Message;
+                }
+            }
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        // connect to database via name
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string nameDatabase = textBoxNameDatabase.Text;
+            ConnectViaStringDTBAsync(nameDatabase);
+        }
+
+        
+
+
     }
 }
